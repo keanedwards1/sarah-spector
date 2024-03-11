6170d072ef32f6f37a9ccb7f310b089eb8a11932
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-03-10 21:18:56 -0700

    completed most of the header and footer html, css, and logic

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,8 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var collapsedMenuList = document.querySelector('.collapsed-menu-list');
+const mobileNav = document.querySelector('.mobile-nav');
+const navLinks = document.querySelectorAll('.nav-links');
+const body = document.querySelector('body');
+const menu = document.querySelector('.menu-toggle');
+const header = document.querySelector('header');
 
-    menuToggle.addEventListener('click', function () {
-        collapsedMenuList.style.display = collapsedMenuList.style.display === 'block' ? 'none' : 'block';
+document.addEventListener('DOMContentLoaded', function () {
+    header.style.marginTop = "-165px";
+    let isMenuOpen = false;
+
+    /* function to toggle mobile nav when menu is clicked */
+
+
+    menu.addEventListener('click', function() {
+        if (isMenuOpen) {
+            header.style.marginTop = "-165px";
+            isMenuOpen = false;
+            document.body.classList.remove('show-menu');
+        } else {
+            header.style.marginTop = "0px";
+            isMenuOpen = true;
+            document.body.classList.add('show-menu');
+        }
+    });
+
+    setTimeout(function() {
+        header.style.transition = "margin-top 0.4s ease";
+    }, 100); // Delay adding the transition by 100ms
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 768) {
+            header.style.marginTop = "-165px";
+            mobileNav.style.zIndex = "-1";
+            isMenuOpen = false;
+            document.body.classList.remove('show-menu');
+        }
     });
 });
+
